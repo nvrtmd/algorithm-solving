b6abb476f69a1beac6a477d03233bb5b15281000
YUZAMIN <untmd.crmn@gmail.com>
2022-06-24 03:04:37 +0900

    DAY_33

---
+++ Basic/no33.py
@@ -0,0 +1,12 @@
+# BOJ 단계별로 풀어보기
+# 3. for문
+
+# 10950
+# t = int(input())
+# num_list = []
+# for i in range(t):
+#     a, b = map(int, input().split())
+#     num_list.append([a, b])
+
+# for i in range(t):
+#     print(num_list[i][0] + num_list[i][1])
