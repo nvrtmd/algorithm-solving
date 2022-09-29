157c3b39116e003873ed382b60ab64cb51ec0121
YUZAMIN <untmd.crmn@gmail.com>
2022-09-29 23:31:14 +0900

    Day 69

--- Basic/Day 069.py
+++ Basic/Day 069.py
@@ -1,28 +1,11 @@
 # BOJ 단계별로 풀어보기
 # 8. 기본 수학 1
 
-# 1193
-x = int(input())
-
-group = 0
-last_num_of_group = 0
-
-while last_num_of_group < x:
-    last_num_of_group += group+1
-    group += 1
-
-num1 = group
-num2 = 1
-
-for i in range(last_num_of_group - x):
-    num1 -= 1
-    num2 += 1
-
-if group % 2 == 0:
-    child = num1
-    mother = num2
+# 2869
+a, b, v = map(int, input().split())
+if (v - b) % (a-b) != 0:
+    result = (v - b) // (a-b) + 1
 else:
-    child = num2
-    mother = num1
+    result = (v - b) // (a-b)
 
-print(f"{child}/{mother}")
+print(result)
