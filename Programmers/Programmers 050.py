796fee9e597d4935c23120c7272b26e5e31b97c0
YUZAMIN <untmd.crmn@gmail.com>
2023-03-16 22:31:35 +0900

    Programmers 050

--- Programmers/Programmers 050.py
+++ Programmers/Programmers 050.py
@@ -1,21 +1,12 @@
-# 디스크 컨트롤러
-import heapq
-
-
-def solution(jobs):
-    answer, job_start_time = 0, 0
-    num_of_jobs = len(jobs)
-    while len(jobs) > 0:
-        jobs_arr = []
-        for job in jobs:
-            if job[0] <= job_start_time:
-                heapq.heappush(jobs_arr, [job[1], job[0]])
-        if len(jobs_arr) < 1:
-            job_start_time += 1
-            continue
-        min_time_job = heapq.heappop(jobs_arr)
-        answer += (job_start_time + min_time_job[0] - min_time_job[1])
-        job_start_time += min_time_job[0]
-        jobs.remove([min_time_job[1], min_time_job[0]])
-
-    return answer // num_of_jobs
+# 정수 삼각형
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i - 1][j]
+            elif j == len(triangle[i]) - 1:
+                triangle[i][j] += triangle[i - 1][j - 1]
+            else:
+                triangle[i][j] += max(triangle[i - 1]
+                                      [j - 1], triangle[i - 1][j])
+    return max(triangle[-1])
