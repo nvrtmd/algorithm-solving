9dd9704382426844f69b2f321a536cc2e246c7fc
YUZAMIN <untmd.crmn@gmail.com>
2023-05-13 23:24:16 +0900

    Programmers 067

---
+++ Programmers/Programmers 067.py
@@ -0,0 +1,12 @@
+# 콜라츠 추측
+def solution(num):
+    answer = 0
+
+    while num != 1:
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num = num * 3 + 1
+        answer += 1
+
+    return answer if answer < 500 else -1
